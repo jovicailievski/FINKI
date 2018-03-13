5b22f688d52f87b93aabfed405d373c65d4be2d9
jovicailievski <jovicailievski97@gmail.com>
2018-03-13 21:09:43 +0100

    Finished Lab2-Zad1

--- Operativni sistemi/Lab2/Zad1/src/TwoThreads.java
+++ Operativni sistemi/Lab2/Zad1/src/TwoThreads.java
@@ -1,3 +1,7 @@
+
+
+
+
 public class TwoThreads {
     public static class Thread1 extends Thread {
         public void run() {
@@ -13,9 +17,39 @@ public class TwoThreads {
         }
     }
 
-    public static void main(String[] args) {
-        new Thread1().start();
-        new Thread2().start();
+
+    static class ThreadAB implements Runnable{
+        private String []chars;
+        public ThreadAB(String chars) {
+            this.chars = chars.split("\\s+");
+        }
+
+        private void printChars(){
+            for(String a: chars){
+                System.out.println(a);
+            }
+        }
+
+
+        @Override
+        public void run() {
+            this.printChars();
+        }
+    }
+
+    public static void main(String[] args){
+        ThreadAB r1 = new ThreadAB("A B C D E F G H I J K L M N O P Q R S T U V W X Y Z ");
+        ThreadAB r2 = new ThreadAB("1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26");
+
+        Thread a = new Thread(r1);
+        Thread b = new Thread(r2);
+        a.start();
+        try {
+            a.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        b.start();
     }
 
 }
